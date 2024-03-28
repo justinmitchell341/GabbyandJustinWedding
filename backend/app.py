504a853634f7f07815519f7a4aa2ffbc1cdc6d31
justinmitchell341 <justin.mitchell341@gmail.com>
2024-03-28 14:13:32 -0700

    Baby Steps

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,22 @@
-from flask import Flask, request, jsonify
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from email import message
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 uri = "mongodb+srv://justinmitchell341:YLszB5RlebeMIRFb@weddingapp.aa8r0ns.mongodb.net/?retryWrites=true&w=majority&appName=WeddingApp"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-
-# @app.route('/submit-rsvp', methods=['POST'])
-# def submit_rsvp():
-#     name = request.form['name']
-#     guests = request.form['guests']
-#     email = request.form['email']
-#     response = request.form['response']
-
+client = MongoClient(uri)
+db = client["WeddingApp"]
+collection = db["RSVPs"]
+@app.route("/submit-rsvp", methods=["POST"])
+def submit_rsvp():
+    data = request.json
+    name = data.get("name")
+    email = data.get("email")
+    respons = data.get("response")
+    guests = data.get("guests")
     
+    return jsonify({message: "RSVP submitted successfully"}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
